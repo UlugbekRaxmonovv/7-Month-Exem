4cff78eb8d4337ca0c2fd56c2221d9737d219a35
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-05-28 02:58:25 +0500

    main

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './Contact.scss'
 import { Link } from 'react-router-dom';
 import rasm1 from '../../assets/img/contact.png'
 import Footer from '../../Components/Footer/Footer';
+const Bot_Token = "6409705996:AAH7HRsbbymTuGEK2h8joN7nQX0Eypu7MRg"
+const Chat_ID = "-1002098227822"
+const User_ID = "5125371890"
 
 const Contact = () => {
+    const [name,setName] = useState('')
+    const [email,setEmail] = useState('')
+    const [textarea,setTextarea] = useState('')
+
+
+
+    const handelSubmit = (e) => {
+        e.preventDefault()
+        let text = "Buyurtma %0A%0A"
+        text +=`Ismi:${name}%0A`
+        text +=`Email:${email}%0A`
+        text +=`Text:${textarea}%0A`
+        
+        let url = `https://api.telegram.org/bot${Bot_Token}/sendMessage?chat_id=${Chat_ID}&text=${text}`
+ 
+        let api = new XMLHttpRequest()
+        api.open("GET", url, true)
+        api.send()
+
+        setName('')
+        setSurname('')
+        setEmail('')
+        setTextarea('')
+    }
     return (
         <div>
            <div className="single">
@@ -34,19 +61,28 @@ Phase 1, Lagos.</h5>
                 </div>
                 <div className="contact_all">
                     <div className="input">
-                        <form action="">
+                        <form action=""  onSubmit={handelSubmit}>
                         <div className="inpu_alt">
                            <label htmlFor="">Fullname</label><br />
-                            <input type="text" placeholder='James Doe' />
+                            <input
+                             value={name}
+                             onChange={(e) =>setName(e.target.value)}
+                            type="text" placeholder='James Doe' />
                            </div>
                            <div className="inpu_alt">
                            <label htmlFor="">Email</label> <br />
-                            <input type="text" placeholder='jamesdoe@gmail.com' />
+                            <input
+                             value={email}
+                             onChange={(e) =>setEmail(e.target.value)}
+                            type="text" placeholder='jamesdoe@gmail.com' />
                            </div>
 
                            <div className="inpu_alt">
                          <label htmlFor="">Message</label> <br />
-                           <textarea name="" id="" cols="30" rows="10">
+                           <textarea 
+                            value={textarea}
+                            onChange={(e) =>setTextarea(e.target.value)}
+                           name="" id="" cols="30" rows="10">
                            </textarea>
                            
                            </div>
