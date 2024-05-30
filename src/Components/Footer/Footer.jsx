6e84d1bf96453a27943bc9d2a9cf717d5bbcbb69
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-05-30 16:08:49 +0500

    main

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -56,6 +56,7 @@ const Footer = () => {
                       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever.Since the 1500s, when an unknown printer.</p>
                       
                     </div>
+                    <h1>Lorimfrnhsikj fpqkrlksvhjk</h1>
                     <div className="footir_row">
                     <h1>Follow Us</h1>
                       <h5>Since the 1500s, when an unknown printer took a galley of type and scrambled.</h5>
